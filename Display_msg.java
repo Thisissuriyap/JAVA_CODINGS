b99d9f2f248adec3de5e0a950b1c6189413ddaeb
psuriya <suriyap0506@gmail.com>
2017-10-18 12:53:02 -0700

    first commit

---
+++ Display_msg.java
@@ -0,0 +1,11 @@
+import java.applet.*;
+import java.awt.*;
+public class Display_msg extends Applet
+{
+public  void paint(Graphics g)
+{
+g.drawString("Hello world",100,100);
+}
+}
+//<applet code=Display_msg width=500 height=500></applet>
+
