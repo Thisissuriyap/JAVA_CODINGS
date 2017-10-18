63b662a75329e8d89abe642be4869e5f816e8628
psuriya <suriyap0506@gmail.com>
2017-10-18 15:48:22 -0700

    Output_in_Appletviewer

---
+++ Output_in_Appletviewer.java
@@ -0,0 +1,10 @@
+import java.applet.*;
+import java.awt.*;
+public class Output_in_Appletviewer extends Applet
+{
+public void paint(Graphics g)
+{
+g.drawString("hello world",100,100);
+}
+}
+//<applet code=Output_in_Appletviewer width=500 height=500></applet>
